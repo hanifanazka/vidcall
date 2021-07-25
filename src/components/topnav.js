3fc3265d7f4e2417bd4a180adaaba54753765920
EC2 Default User <ec2-user@ip-172-31-24-71.us-east-2.compute.internal>
2021-07-25 08:37:33 +0000

    Login button coming soon notice

--- src/components/topnav.js
+++ src/components/topnav.js
@@ -1,11 +1,29 @@
 import * as React from "react"
-import { Navbar, Nav } from "react-bootstrap"
+import { Navbar, Nav, Tooltip, Overlay } from "react-bootstrap"
+
+const Login = () => {
+  const [show, setShow] = React.useState(false);
+  const target = React.useRef(null);
+
+  return (
+    <>
+      <Nav.Link ref={target} onClick={() => setShow(!show)} onBlur={() => setShow(false)}>Login</Nav.Link>
+      <Overlay target={target.current} show={show} placement="bottom">
+        {(props) => (
+          <Tooltip id="overlay-example" {...props}>
+            Coming Soon
+          </Tooltip>
+        )}
+      </Overlay>
+    </>
+  );
+}
 
 const TopNav = () => (
   <Navbar>
     <Navbar.Brand href="/">Vidcall</Navbar.Brand>
     <Navbar.Collapse className="justify-content-end">
-      <Nav.Link href="/">Login</Nav.Link>
+      <Login/>
     </Navbar.Collapse>
   </Navbar>
 )
